716103ddc7c4d6c6c55049558eb170e84258593e
cinta <cintatafur@gmail.com>
2023-03-08 12:52:13 +0100

    💚 Inyectamos(Servicio):Muestro por consola el array de los Posts del back

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  constructor(private postsService: PostsService) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    /*     throw new Error('Method not implemented.'); */
 
+    this.postsService.getPosts().subscribe((resp) => {
+      console.log(resp);
+    });
+  }
 }
