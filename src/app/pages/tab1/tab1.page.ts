77271ac87e7aba7cc5d7e8f57eba9ffcfc2cc027
cinta <cintatafur@gmail.com>
2023-03-09 13:55:31 +0100

    ⚡️ Refresh(reloj)

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -8,19 +8,26 @@ import { PostsService } from '../../services/posts.service';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
-
   //ALmacenar los post
-  posts : Post [] | any = [];
+  posts: Post[] | any = [];
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
+    this.siguiente();
     /*     throw new Error('Method not implemented.'); */
-
+  }
+  siguiente(event?: any) {
     this.postsService.getPosts().subscribe((resp) => {
-      console.log(resp.posts);
-      console.log(resp.posts[0].mensaje);
-      this.posts.push(...resp.posts)
+      this.posts.push(...resp.posts);
+
+      if (event) {
+        event.target.complete();
+
+        if (resp.posts.length === 0) {
+          event.target.disabled = true;
+        }
+      }
     });
   }
 }
