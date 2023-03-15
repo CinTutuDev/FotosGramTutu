d8c96ecb58e367841a65b9b9bb5fcb5900c1a4bc
cinta <cintatafur@gmail.com>
2023-03-15 10:08:46 +0100

    ✏️See(avatar):saco el avatar selec en tab3

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -10,32 +10,32 @@ import { UiServiceService } from '../../services/ui-service.service';
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page implements OnInit {
-  /* private para evitar ser leido e fuera */
+  /* private para evitar ser leido e fuera en el src\app\services\usuario.service.ts*/
   user: Usuario = {};
 
-  constructor(private usuarioService: UsuarioService, private uiServide: UiServiceService) {}
+  constructor(
+    private usuarioService: UsuarioService,
+    private uiServide: UiServiceService
+  ) {}
 
   ngOnInit() {
-
     this.user = this.usuarioService.getUusario();
     console.log(this.user);
   }
 
-
-  
-  async actualizar( fActualizar: NgForm ) {
-
-    if ( fActualizar.invalid ) { return; }
-
-    const actualizado = await this.usuarioService.actulizarUser( this.user );
-    if ( actualizado ) {
-      // toast con el mensaje de actualizado
-      this.uiServide.presentToast('middle', 'Registro actualizado' );
-    } else {
-      // toast con el error
-      this.uiServide.presentToast('middle', 'No se pudo actualizar' );
+  async actualizar(fActualizar: NgForm) {
+    if (fActualizar.invalid) {
+      return;
     }
 
+    const actualizado = await this.usuarioService.actulizarUser(this.user);
+    if (actualizado) {
+      // toast con el mensaje de actualizado
+      this.uiServide.presentToast('middle', 'Registro actualizado');
+    } else {
+      // toast con el error
+      this.uiServide.presentToast('middle', 'No se pudo actualizar');
+    }
   }
 
   logout() {}
