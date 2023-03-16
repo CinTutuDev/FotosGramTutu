32573613827d67224ef1887e295d8dc75e79e92b
cinta <cintatafur@gmail.com>
2023-03-16 10:21:30 +0100

    🐛 error(coords):resuelto... coords: null as string | null,

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PostsService } from '../../services/posts.service';
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import { stdin } from 'process';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -12,11 +13,15 @@ export class Tab2Page {
   cargandoGeo = false;
   post = {
     mensaje: '',
-    coords: null,
+    coords: null as string | null,
     posicion: false,
   };
 
-  constructor(private postService: PostsService, private route: Router, private geoLocation: Geolocation) {}
+  constructor(
+    private postService: PostsService,
+    private route: Router,
+    private geoLocation: Geolocation
+  ) {}
 
   async crearPost() {
     console.log(this.post);
@@ -37,20 +42,22 @@ export class Tab2Page {
       this.post.coords = null;
       return;
     }
-    
+
     this.cargandoGeo = true;
 
-    this.geoLocation.getCurrentPosition().then((resp)=>{
-      this.cargandoGeo = false;
-      const coords = `${resp.coords.latitude},${resp.coords.longitude}`;
-      console.log(coords);
-
-    }).catch((error)=>{
-      console.log('Error de coger localización', error);
-      this.cargandoGeo = false;
-
-    })
-
-   /*  console.log(this.post); */
+    this.geoLocation
+      .getCurrentPosition()
+      .then((resp: any) => {
+        this.cargandoGeo = false;
+        const coords = `${resp.coords.latitude},${resp.coords.longitude}`;
+        console.log(coords);
+        this.post.coords = coords;
+      })
+      .catch((error) => {
+        console.log('Error al obtener la ubicación', error);
+        this.cargandoGeo = false;
+        this.post.coords = null;
+      });
+    /*  console.log(this.post); */
   }
 }
