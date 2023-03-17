3cab0117e9c02ed680df317391b7ad2bac1b3b68
cinta <cintatafur@gmail.com>
2023-03-17 09:27:35 +0100

    ✅ Mapa(token):parece que funciona

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,19 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+declare let mapboxgl: any;
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.scss'],
 })
-export class MapaComponent  implements OnInit {
-
-
+export class MapaComponent implements OnInit {
   @Input() coords: string | undefined;
-
-  constructor() { }
+  @ViewChild('mapa', { static: true }) mapa: any;
+  constructor() {}
 
   ngOnInit() {
-    console.log(this.coords);
-  }
+    if (!this.coords) {
+      console.error('Las coordenadas no están definidas');
+      return;
+    }
+    const latLong = this.coords.split(',');
+    if (latLong.length !== 2) {
+      console.error('El formato de las coordenadas no es correcto');
+      return;
+    }
+    const lat = parseFloat(latLong[0]);
+    const long = parseFloat(latLong[1]);
+    if (isNaN(lat) || isNaN(long)) {
+      console.error('Las coordenadas no se pueden convertir a números');
+      return;
+    }
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiY2ludGF0YyIsImEiOiJjbGZjOXVzMnAwbXhmM3huMTc3cnd2cG5rIn0.wCsVZJ0fTk5JtliChD0V5Q';
+    const map = new mapboxgl.Map({
+      container: this.mapa.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [long, lat], // starting position [lng, lat]
+      zoom: 15, // starting zoom
+    });
 
+    /* Creacion de marcador */
+    const marker = new mapboxgl.Marker().setLngLat([long, lat]).addTo(map);
+  }
 }
