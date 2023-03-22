b7672d4bfba4e6b7241a8578b4f9abd628a1bbd9
cinta <cintatafur@gmail.com>
2023-03-22 12:07:53 +0100

    ✨ Change(IP):enviroments-->ipconfig

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -4,7 +4,12 @@ import { environment } from '../../environments/environment';
 import { RespuestaPosts, Post } from '../interfaces/interfaces';
 import { UsuarioService } from './usuario.service';
 import { Platform } from '@ionic/angular';
-import { Camera, CameraResultType, CameraSource , Photo} from '@capacitor/camera';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
 import { Capacitor } from '@capacitor/core';
 import { Preferences } from '@capacitor/preferences';
 import { Filesystem, Directory } from '@capacitor/filesystem';
@@ -14,10 +19,9 @@ const URL = environment.url;
   providedIn: 'root',
 })
 export class PostsService {
-
-  public photos: UserPhoto[]  | any= [];
-  private PHOTO_STORAGE: string | any= 'photos';
-  private platform: Platform ;
+  public photos: UserPhoto[] | any = [];
+  private PHOTO_STORAGE: string | any = 'photos';
+  private platform: Platform;
 
   paginasgPosts = 0;
 
@@ -27,7 +31,9 @@ export class PostsService {
     private http: HttpClient,
     private usuarioService: UsuarioService,
     platform: Platform
-  ) {  this.platform = platform;}
+  ) {
+    this.platform = platform;
+  }
 
   getPosts(pull: boolean = false) {
     if (pull) {
@@ -58,17 +64,14 @@ export class PostsService {
     });
   }
 
-
-
-
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
-      quality: 100
+      quality: 100,
     });
- 
+
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
@@ -82,15 +85,15 @@ export class PostsService {
   private async savePicture(photo: Photo) {
     // Convert photo to base64 format, required by Filesystem API to save
     const base64Data = await this.readAsBase64(photo);
-  
+
     // Write the file to the data directory
     const fileName = new Date().getTime() + '.jpeg';
     const savedFile = await Filesystem.writeFile({
       path: fileName,
       data: base64Data,
-      directory: Directory.Data
+      directory: Directory.Data,
     });
-  
+
     if (this.platform.is('hybrid')) {
       // Display the new image by rewriting the 'file://' path to HTTP
       // Details: https://ionicframework.com/docs/building/webview#file-protocol
@@ -98,31 +101,30 @@ export class PostsService {
         filepath: savedFile.uri,
         webviewPath: Capacitor.convertFileSrc(savedFile.uri),
       };
-    }
-    else {
+    } else {
       // Use webPath to display the new image instead of base64 since it's
       // already loaded into memory
       return {
         filepath: fileName,
-        webviewPath: photo.webPath
+        webviewPath: photo.webPath,
       };
     }
   }
 
   public async loadSaved() {
     // Retrieve cached photo array data
-    const photoList  = await Preferences.get({ key: this.PHOTO_STORAGE });
+    const photoList = await Preferences.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photoList.value ?? '[]');
-  
+
     if (!this.platform.is('hybrid')) {
       // Display the photo by reading into base64 format
       for (let photo of this.photos) {
         // Read each saved photo's data from the Filesystem
         const readFile = await Filesystem.readFile({
-            path: photo.filepath,
-            directory: Directory.Data
+          path: photo.filepath,
+          directory: Directory.Data,
         });
-  
+
         // Web platform only: Load the photo as base64 data
         photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
       }
@@ -131,20 +133,19 @@ export class PostsService {
   public async deletePicture(photo: UserPhoto, position: number) {
     // Remove this photo from the Photos reference data array
     this.photos.splice(position, 1);
-  
+
     // Update photos array cache by overwriting the existing photo array
     Preferences.set({
       key: this.PHOTO_STORAGE,
-      value: JSON.stringify(this.photos)
+      value: JSON.stringify(this.photos),
     });
-  
+
     // delete photo file from filesystem
-    const filename = photo.filepath
-                        .substr(photo.filepath.lastIndexOf('/') + 1);
-  
+    const filename = photo.filepath.substr(photo.filepath.lastIndexOf('/') + 1);
+
     await Filesystem.deleteFile({
       path: filename,
-      directory: Directory.Data
+      directory: Directory.Data,
     });
   }
   private async readAsBase64(photo: Photo) {
@@ -152,31 +153,29 @@ export class PostsService {
     if (this.platform.is('hybrid')) {
       // Read the file into base64 format
       const file = await Filesystem.readFile({
-        path: photo.path!
+        path: photo.path!,
       });
-  
+
       return file.data;
-    }
-    else {
+    } else {
       // Fetch the photo, read as a blob, then convert to base64 format
       const response = await fetch(photo.webPath!);
       const blob = await response.blob();
-  
-      return await this.convertBlobToBase64(blob) as string;
+
+      return (await this.convertBlobToBase64(blob)) as string;
     }
   }
-  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = reject;
-    reader.onload = () => {
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
         resolve(reader.result);
-    };
-    reader.readAsDataURL(blob);
-  });
-  
+      };
+      reader.readAsDataURL(blob);
+    });
 }
 export interface UserPhoto {
   filepath: string;
   webviewPath: string;
 }
-
